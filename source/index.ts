1b9449f24d805cbdaaf253c42ab5386bfa751459
Joshua Miller <joshdan65@gmail.com>
2020-03-22 13:58:15 -0500

    Added handling of paging

--- source/index.ts
+++ source/index.ts
@@ -12,16 +12,35 @@ async function doStuff() {
 
   await page.addScriptTag({ url: 'https://code.jquery.com/jquery-3.2.1.min.js' })
 
+  let buttonHandle = await page.$(".options__load-more");
+
+  let isNotHidden = await page.$eval('.options__load-more', (elem) => {
+    return window.getComputedStyle(elem).getPropertyValue('display') !== 'none';
+  });
+
+  while (buttonHandle !== null && isNotHidden) {
+
+    await sleep(500).then(() => {
+      buttonHandle!.click();
+      console.log("Clicked");
+    });
+
+    buttonHandle = await page.$(".options__load-more");
+    isNotHidden = await page.$eval('.options__load-more', (elem) => {
+      return window.getComputedStyle(elem).getPropertyValue('display') !== 'none';
+    });
+  }
+
   const buttonQuery = ".audio-tool.audio-tool-download > a";
   const buttonLinks = await page.$$eval(buttonQuery, el => el.map(x => x.getAttribute("href")));
 
-  buttonLinks.forEach(link => {
-    const notNullLink = link!;
-    const fileName = getFileName(notNullLink);
-    const folderPath = "E:/CarTalks/";
-    console.log(fileName);
-    downloadAudioFromLink(notNullLink, fileName, folderPath);
-  });
+    // buttonLinks.forEach(link => {
+  //   const notNullLink = link!;
+  //   const fileName = getFileName(notNullLink);
+  //   const folderPath = "E:/CarTalks/";
+  //   console.log(fileName);
+  //   downloadAudioFromLink(notNullLink, fileName, folderPath);
+  // });
 
   console.log(buttonLinks.length);
 }
@@ -38,6 +57,10 @@ function getFileName(rawLink: string) {
   const lastIndexofSlash = partialSubstring.lastIndexOf("/");
   const fileName = partialSubstring.substring(lastIndexofSlash + 1) + ".mp3";
   return fileName;
+}
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 doStuff();
